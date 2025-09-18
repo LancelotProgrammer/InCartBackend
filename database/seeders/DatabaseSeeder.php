740bc22dab9e486ace194c6a3aeeac0eaef908b2
LancelotProgrammer <suhail.01248@gmail.com>
2025-09-18 18:28:23 +0300

    chore: clean the code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,19 +10,19 @@ class DatabaseSeeder extends BaseSeeder
     public function run(): void
     {
         $this->generateData(
-            rootCategoryCount :  3,
-            productCount :  200,
-            productCategoryCount :  10,
-            branchAdvertisementCount :  30,
-            userCount :  100,
-            userAddressCount :  3,
-            userNotificationCount :  3,
-            favoriteCount :  3,
-            packageProductCount :  10,
-            productFileCount :  3,
-            categoryFileCount :  1,
-            advertisementFileCount :  1,
-            orderCount :  200
+            rootCategoryCount: 3,
+            productCount: 200,
+            productCategoryCount: 10,
+            branchAdvertisementCount: 30,
+            userCount: 100,
+            userAddressCount: 3,
+            userNotificationCount: 3,
+            favoriteCount: 3,
+            packageProductCount: 10,
+            productFileCount: 3,
+            categoryFileCount: 1,
+            advertisementFileCount: 1,
+            orderCount: 200
         );
     }
 }
