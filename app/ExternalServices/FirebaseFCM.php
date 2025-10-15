63a14d0acf2ebdfd8e02da79e84e9db02c82e331
LancelotProgrammer <suhail.01248@gmail.com>
2025-10-16 00:23:06 +0300

    fix: update TICKET_DEEP_LINK to include specific support ticket ID

--- app/ExternalServices/FirebaseFCM.php
+++ app/ExternalServices/FirebaseFCM.php
@@ -15,7 +15,7 @@ class FirebaseFCM
 {
     public const ORDER_DEEP_LINK = '/order-details';
 
-    public const TICKET_DEEP_LINK = '/support';
+    public const TICKET_DEEP_LINK = '/support/1';
 
     public const PRODUCT_DEEP_LINK = '/product-details';
 
