8281d4e4416c65a4d9467e2904e6e15d0de7c3e1
LancelotProgrammer <suhail.01248@gmail.com>
2025-11-13 00:46:05 +0300

    feat: set default sort order for products table by id in descending order

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -14,6 +14,7 @@ class ProductsTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->defaultSort('id', 'desc')
             ->defaultPaginationPageOption(25)
             ->contentGrid([
                 'sm' => 1,
