2c3c4248ccd8d5f32bd77b4109c8601f20e41ee5
LancelotProgrammer <suhail.01248@gmail.com>
2025-11-25 20:15:07 +0300

    chore: delete settings cache when domain event fires for setting model

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use App\Enums\SettingType;
+use App\Services\CacheService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Facades\Log;
 use OwenIt\Auditing\Auditable;
 use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 use OwenIt\Auditing\Models\Audit;
@@ -23,6 +25,22 @@ class Setting extends Model implements AuditableContract
     ];
 
     protected $auditInclude = ['key', 'value', 'type', 'group', 'is_locked'];
+
+    protected static function booted(): void
+    {
+        static::created(function (Setting $setting) {
+            Log::channel('app_log')->info('Models: created new setting and deleted settings cache.', ['id' => $setting->id]);
+            return CacheService::deleteSettingsCache();
+        });
+        static::updated(function (Setting $setting) {
+            Log::channel('app_log')->info('Models: updated setting and deleted settings cache.', ['id' => $setting->id]);
+            return CacheService::deleteSettingsCache();
+        });
+        static::deleted(function (Setting $setting) {
+            Log::channel('app_log')->info('Models: deleted setting and deleted settings cache.', ['id' => $setting->id]);
+            return CacheService::deleteSettingsCache();
+        });
+    }
 
     public function audits(): MorphMany
     {
